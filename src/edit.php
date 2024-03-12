0f787deabfd0cfc96656d65c46e7d6ec2a2a0000
Kuushtaa91 <jesus.cuestagomez@iesmiguelherrero.com>
2024-03-12 18:57:07 +0100

    modifico

--- src/edit.php
+++ src/edit.php
@@ -8,33 +8,44 @@ Transacción de datos utilizando el método: POST
 */
 if(isset($_POST['modifica'])) {
 	$id = mysqli_real_escape_string($mysqli, $_POST['id']);
-	$name = mysqli_real_escape_string($mysqli, $_POST['name']);
-	$surname = mysqli_real_escape_string($mysqli, $_POST['surname']);
-	$age = mysqli_real_escape_string($mysqli, $_POST['age']);
+	$tit = mysqli_real_escape_string($mysqli, $_POST['tit']);
+	$lan = mysqli_real_escape_string($mysqli, $_POST['lan']);
+	$ep = mysqli_real_escape_string($mysqli, $_POST['ep']);
+	$est = mysqli_real_escape_string($mysqli, $_POST['est']);
+	$continua = mysqli_real_escape_string($mysqli, $_POST['continua']);
 
-	if(empty($name) || empty($surname) || empty($age))	{
-		if(empty($name)) {
+	if(empty($tit) || empty($lan) || empty($ep) || empty($est) | empty($continua))	{
+		if(empty($tit)) {
 			echo "<font color='red'>Campo nombre vacío.</font><br/>";
 		}
 
-		if(empty($surname)) {
+		if(empty($lan)) {	
 			echo "<font color='red'>Campo apellido vacío.</font><br/>";
 		}
 
-		if(empty($age)) {
+		if(empty($ep)) {
 			echo "<font color='red'>Campo edad vacío.</font><br/>";
 		}
+
+		if(empty($est)) {
+			echo "<font color='red'>Campo edad vacío.</font><br/>";
+		}
+
+		if(empty($continua)) {
+			echo "<font color='red'>Campo edad vacío.</font><br/>";
+		}
+
 	} //fin si
 	else 
 	{
 //Prepara una sentencia SQL para su ejecución. En este caso una modificación de un registro de la BD.				
-		$stmt = mysqli_prepare($mysqli, "UPDATE users SET name=?,surname=?,age=? WHERE id=?");
+		$stmt = mysqli_prepare($mysqli, "UPDATE anime SET tit=?,lan=?,ep=?,est=?,continua=? WHERE id=?");
 /*Enlaza variables como parámetros a una setencia preparada. 
 i: La variable correspondiente tiene tipo entero
 d: La variable correspondiente tiene tipo doble
 s:	La variable correspondiente tiene tipo cadena
 */				
-		mysqli_stmt_bind_param($stmt, "ssii", $name, $surname, $age, $id);
+		mysqli_stmt_bind_param($stmt, "ssissi", $tit, $lan, $ep, $est, $continua, $id);
 //Ejecuta una consulta preparada			
 		mysqli_stmt_execute($stmt);
 //Libera la memoria donde se almacenó el resultado
@@ -56,13 +67,13 @@ $id = mysqli_real_escape_string($mysqli, $id);
 
 
 //Prepara una sentencia SQL para su ejecución. En este caso selecciona el registro a modificar y lo muestra en el formulario.				
-$stmt = mysqli_prepare($mysqli, "SELECT name, surname, age FROM users WHERE id=?");
+$stmt = mysqli_prepare($mysqli, "SELECT tit, lan, ep, est, continua FROM anime WHERE id=?");
 //Enlaza variables como parámetros a una setencia preparada. 
 mysqli_stmt_bind_param($stmt, "i", $id);
 //Ejecuta una consulta preparada
 mysqli_stmt_execute($stmt);
 //Enlaza variables a una setencia preparada para el almacenamiento del resultado
-mysqli_stmt_bind_result($stmt, $name, $surname, $age);
+mysqli_stmt_bind_result($stmt, $tit, $lan, $ep, $est, $continua);
 //Obtiene el resultado de una sentencia SQL preparada en las variables enlazadas
 mysqli_stmt_fetch($stmt);
 //Libera la memoria donde se almacenó el resultado		
@@ -78,7 +89,7 @@ mysqli_close($mysqli);
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">	
-	<title>Modificación trabajador/a</title>
+	<title>Modificación anime </title>
 <!--	
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 -->
@@ -98,23 +109,33 @@ mysqli_close($mysqli);
 		<li><a href="index.php" >Inicio</a></li>
 		<li><a href="add.html" >Alta</a></li>
 	</ul>
-	<h2>Modificación trabajador/a</h2>
+	<h2>Modificación anime</h2>
 <!--Formulario de edición. 
 Al hacer click en el botón Guardar, llama a esta misma página: edit.php-->
 	<form action="edit.php" method="post">
 		<div>
-			<label for="name">Nombre</label>
-			<input type="text" name="name" id="name" value="<?php echo $name;?>" required>
+			<label for="tit">Titulo</label>
+			<input type="text" name="tit" id="tit" value="<?php echo $tit;?>" required>
 		</div>
 
 		<div>
-			<label for="surname">Apellido</label>
-			<input type="text" name="surname" id="surname" value="<?php echo $surname;?>" required>
+			<label for="lan">Lanzamiento</label>
+			<input type="text" name="lan" id="lan" value="<?php echo $lan;?>" required>
 		</div>
 
 		<div>
-			<label for="age">Edad</label>
-			<input type="number" name="age" id="age" value="<?php echo $age;?>" required>
+			<label for="ep">Episodios</label>
+			<input type="number" name="ep" id="ep" value="<?php echo $ep;?>" required>
+		</div>
+
+		<div>
+			<label for="est">Estudio</label>
+			<input type="text" name="est" id="est" value="<?php echo $est;?>" required>
+		</div>
+
+		<div>
+			<label for="continua">Continua</label>
+			<input type="text" name="continua" id="continua" value="<?php echo $continua;?>" required>
 		</div>
 
 		<div >
@@ -126,7 +147,7 @@ Al hacer click en el botón Guardar, llama a esta misma página: edit.php-->
 
 	</main>	
 	<footer>
-	Created by the IES Miguel Herrero team &copy; 2024
+	Created by the jesus cuesta gomez sin copiar a nadie &copy; 2024
   	</footer>
 </div>
 </body>
